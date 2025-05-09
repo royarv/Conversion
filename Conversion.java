079b9b6f93ddd5d9737b5ed122ee2aa77ef503fe
royarv <20243rd054@utez.edu.mx>
2025-05-09 10:19:36 -0600

    Se creo un archivo

---
+++ Conversion.java
@@ -0,0 +1,8 @@
+public class Conversion{
+    public static void main(String[] arg){
+        int numero1 = 3;
+        double numero2 = 3.7;
+        double resultado = numero1 + numero2;
+        System.out.println(resultado);
+    }
+    }
